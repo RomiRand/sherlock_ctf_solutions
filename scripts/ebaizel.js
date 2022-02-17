acb25ce15a2c31927fa8d65997acec8ee32a805b
RamiRond <raami.roond@gmail.com>
2022-02-17 08:41:02 +0100

    ebaizel

---
+++ scripts/ebaizel.js
@@ -0,0 +1,25 @@
+const { parseEther } = require("ethers/lib/utils");
+
+async function main() {
+  // const [owner, attacker, addr2] = await ethers.getSigners();
+  const SETUP = await ethers.getContractFactory("contracts/ebaizel/Setup.sol:Setup");
+  const EXPLOIT = await ethers.getContractFactory("contracts/ebaizel/Exploit.sol:Exploit");
+
+  const setup = await SETUP.deploy({ value: 298 });
+  const victim = await ethers.getContractAt(
+    "PixelPavel",
+    await setup.instance()
+  );
+
+  console.log("solved:", await setup.isSolved());
+  exploit = await EXPLOIT.deploy(victim.address);
+  await exploit.exploit();
+  console.log("solved:", await setup.isSolved());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
