b922da54b13c931c559028a1ac1bd7fafce793b9
RamiRond <raami.roond@gmail.com>
2022-02-18 16:58:27 +0100

    older oz version added

--- contracts/agusduha/TheKingIsDeadLongLiveTheKing.sol
+++ contracts/agusduha/TheKingIsDeadLongLiveTheKing.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable-4/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable-4/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable-4/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
